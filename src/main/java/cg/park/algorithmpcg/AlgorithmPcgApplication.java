bc0245622adbf6a89ae5449d5f65977a089f2098
changyun <qkrcksrbs8@naver.com>
2022-03-19 23:42:55 +0900

    PCG의 알고리즘 테스트

---
+++ src/main/java/cg/park/algorithmpcg/AlgorithmPcgApplication.java
@@ -0,0 +1,13 @@
+package cg.park.algorithmpcg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmPcgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmPcgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cg/park/algorithmpcg/AlgorithmPcgApplicationTests.java
@@ -0,0 +1,13 @@
+package cg.park.algorithmpcg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmPcgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
