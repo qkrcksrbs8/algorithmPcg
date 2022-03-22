6f16c43828d7bd17efcab42dbcf048a04af10bee
changyun <qkrcksrbs8@naver.com>
2022-03-22 14:51:06 +0000

    [백준][1874] 스택 수열

--- src/main/java/cg/park/algorithmpcg/baekjoon/stack/_1874.java
+++ src/main/java/cg/park/algorithmpcg/baekjoon/stack/_1874.java
@@ -1,27 +1,47 @@
 package cg.park.algorithmpcg.baekjoon.stack;
 
-import java.util.Arrays;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 // 스택 수열 1874 문제
 // https://www.acmicpc.net/problem/1874
 public class _1874 {
-    static Stack<Integer> stack = new Stack<>();
-    static int cnt = 0;
 
-    public static void toStack(String command) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        if ("push".equals(command)) {
-            cnt++;
-            stack.push(cnt);
-            return;
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+
+        int[] stack = new int[N];
+
+        int idx = 0;
+        int start = 0;
+
+        // N 번 반복
+        while(N -- > 0) {
+            int value = Integer.parseInt(br.readLine());
+
+            if(value > start) {
+                // start + 1부터 입력받은 value 까지 push를 한다.
+                for(int i = start + 1; i <= value; i++) {
+                    stack[idx] = i;
+                    idx++;
+                    sb.append('+').append('\n');
+                }
+                start = value; 	// 다음 push 할 때의 오름차순을 유지하기 위한 변수 초기화
+            }
+
+            // top에 있는 원소가 입력받은 값과 같이 않은 경우
+            else if(stack[idx - 1] != value) {
+                System.out.println("NO");
+                System.exit(0);	//	return 으로 대체해도 됨
+            }
+
+            idx--;
+            sb.append('-').append('\n');
         }
-        System.out.println(stack.pop());
-    }
-
-    public static void main(String[] args) {
-        String[] arrs = {"push", "push", "push", "push", "pop", "pop", "push", "push", "pop", "pop"};
-
-        Arrays.stream(arrs).forEach(arr ->  {toStack(arr);});
+        System.out.println(sb);
     }
 }
