cb4e095438e08452bac2bf31f2d6f95ade844833
changyun <qkrcksrbs8@naver.com>
2022-03-23 14:11:47 +0000

    [백준][10866] 풀이 중

---
+++ src/main/java/cg/park/algorithmpcg/baekjoon/deque/_10866.java
@@ -0,0 +1,18 @@
+package cg.park.algorithmpcg.baekjoon.deque;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class _10866 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = Integer.parseInt(br.readLine());
+        while(cnt -- > 1 ){
+            System.out.println(cnt);
+        }
+
+    }
+
+}
