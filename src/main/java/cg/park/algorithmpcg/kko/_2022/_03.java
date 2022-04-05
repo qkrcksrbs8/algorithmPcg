6fb7a124db35da2df6ec73e011e6a333c0a193f1
changyun <qkrcksrbs8@naver.com>
2022-04-05 13:51:37 +0100

    [카카오][2022_03] 진행 중2

--- src/main/java/cg/park/algorithmpcg/kko/_2022/_03.java
+++ src/main/java/cg/park/algorithmpcg/kko/_2022/_03.java
@@ -1,35 +1,58 @@
 package cg.park.algorithmpcg.kko._2022;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class _03 {
 
-    public static boolean isPrime(Long n) {
-        if(n < 2) return false;
-        if(n == 2) return true;
-        for (int i = 2; i <= Math.sqrt(n); i++) if(n % i == 0) return false;
-        return true;
-    }
+    static HashMap<String, String> inoutMap = new HashMap<>();
+    static HashMap<String, Integer> sumMap = new HashMap<>();
 
-    public static Long toEverynary (int n, int k) {
-        return Long.parseLong(Integer.toString(n,k));
-    }
-
-    public static int solution(int n, int k) {
-        int answer = 0;
-        StringTokenizer token = new StringTokenizer(Long.toString(toEverynary(n, k)), "0");
-        while (token.countTokens() > 0) {
-            if (isPrime(Long.parseLong(token.nextToken()))) answer++;
+    public static int[] getAnswer(int[] fees) {
+        int[] answer = new int[sumMap.size()];
+        //기본 시간(분)	기본 요금(원)	단위 시간(분)	단위 요금(원)
+        int defaultHour = fees[0];
+        int defaultFee = fees[1];
+        int defaultMinute = fees[2];
+        int unit = fees[3];
+        int index = 0;
+        for (Map.Entry<String, Integer> entry : sumMap.entrySet()) {
+            answer[index] = (defaultHour >= entry.getValue())
+                            ? defaultFee
+                            : defaultFee + ((entry.getValue() - defaultHour) / defaultMinute) * unit;
+            index++;
         }
         return answer;
     }
 
+    public static int getParkingTime(StringTokenizer token) {
+        return (Integer.parseInt(token.nextToken()) * 60) + (Integer.parseInt(token.nextToken()));
+    }
+
+    public static int sumParkingTime(String carNo) {
+        return getParkingTime(new StringTokenizer(inoutMap.get(carNo), ":"));
+    }
+
+    public static void toInOut(String[] history) {
+        if ("IN".equals(history[2])) {
+            inoutMap.put((history[1]), history[0]);
+            return;
+        }
+        if (sumMap.containsKey((history[1])));
+        sumMap.put((history[1]), sumMap.getOrDefault(history[1], 0) + sumParkingTime(history[1]));
+    }
+
+    public static int[] solution(int[] fees, String[] records) {
+        Arrays.stream(records).forEach(record -> { toInOut(record.split(" ")); });
+        return getAnswer(fees);
+    }
+
     public static void main(String[] args) {
-        int n = 110011;
-        int k = 10;
-//        int n = 437674;
-//        int k = 3;
-        System.out.println(solution(n,k));
+        int[] fees = {180, 5000, 10, 600};
+        String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
+        System.out.println(solution(fees, records));
     }
 
 }
