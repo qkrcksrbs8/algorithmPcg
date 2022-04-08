03021561f896dd672616b36cba7ec700eaa47af6
changyun <qkrcksrbs8@naver.com>
2022-04-08 15:54:51 +0900

    자바 테스트파일

---
+++ src/main/java/cg/park/algorithmpcg/baekjoon/MyDate.java
@@ -0,0 +1,25 @@
+package cg.park.algorithmpcg.baekjoon;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class MyDate {
+    private int day = 0;
+    private int month = 0;
+    private int year = 0;
+    public MyDate(int day) {this.day = day;};
+}
+
+
+class test2 extends MyDate{
+    public test2(int day) {
+        super(day);
+    }
+
+    public void ttt() {
+
+    }
+
+}
--- src/main/java/cg/park/algorithmpcg/baekjoon/test.java
+++ src/main/java/cg/park/algorithmpcg/baekjoon/test.java
@@ -1,7 +1,44 @@
 package cg.park.algorithmpcg.baekjoon;
 
-public class test {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
+public class test {
+
+    private int day = 0;
+    private int month = 0;
+    private int year = 0;
+
+    test() {}
+    test(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
 }
