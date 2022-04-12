4e43c3cd6062c43d699293f9e03eceef4d8a579b
changyun <qkrcksrbs8@naver.com>
2022-04-13 01:04:42 +0900

    [카카오]2022_03 어디지

--- src/main/java/cg/park/algorithmpcg/kko/_2022/_03.java
+++ src/main/java/cg/park/algorithmpcg/kko/_2022/_03.java
@@ -8,7 +8,7 @@ import java.util.StringTokenizer;
 //https://programmers.co.kr/learn/courses/30/lessons/92341
 public class _03 {
 
-    static HashMap<String, String> inoutMap = new HashMap<>();
+    static HashMap<String, Integer> inoutMap = new HashMap<>();
     static HashMap<String, Integer> sumMap = new HashMap<>();
 
     public static int[] getAnswer(int[] fees) {
@@ -28,32 +28,47 @@ public class _03 {
         return answer;
     }
 
-    public static int getParkingTime(StringTokenizer token) {
+    public static int secondParkingTime(StringTokenizer token) {
         return (Integer.parseInt(token.nextToken()) * 60) + (Integer.parseInt(token.nextToken()));
     }
 
-    public static int sumParkingTime(String carNo) {
-        return getParkingTime(new StringTokenizer(inoutMap.get(carNo), ":"));
+    public static int toParkingTime(String time) {
+        return secondParkingTime(new StringTokenizer(time, ":"));
     }
 
     public static void toInOut(String[] history) {
         if ("IN".equals(history[2])) {
-            inoutMap.put((history[1]), history[0]);
+            inoutMap.put((history[1]), toParkingTime(history[0]));
+            sumMap.put((history[1]), 0);
             return;
         }
-        if (sumMap.containsKey((history[1])));
-        sumMap.put((history[1]), sumParkingTime(history[1]) - sumMap.getOrDefault(history[1], 0));
+        int enterTime =  inoutMap.get(history[1]);// 입장 시간
+        int exitTime = toParkingTime(history[0]);// 종료 시간
+        int playTime = exitTime - enterTime;// 이용 시간
+        int sumTime = sumMap.get(history[1]);// 누적 이용 시간
+        sumMap.put(history[1], playTime + sumTime);
+        inoutMap.remove(history[1]);
+        //sumMap.put(toParkingTime(history[0]) - inoutMap.get(history[1]) + sumMap.getOrDefault(history[1], 0));// 위의 변수 대신 이렇게 한 줄로 사용 가능
     }
 
     public static int[] solution(int[] fees, String[] records) {
         Arrays.stream(records).forEach(record -> { toInOut(record.split(" ")); });
+        if (!inoutMap.isEmpty()) {
+            for (Map.Entry<String, Integer> map : inoutMap.entrySet()){
+                int sumTime = sumMap.getOrDefault(map.getKey(), 0);// 누적 이용 시간
+                sumMap.put(map.getKey(), 1439  - map.getValue() + sumTime);
+            }
+        }
         return getAnswer(fees);
     }
 
     public static void main(String[] args) {
         int[] fees = {180, 5000, 10, 600};
         String[] records = {"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"};
-        System.out.println(solution(fees, records));
+//        int[] fees = {1, 461, 1, 10};
+//        String[] records = {"00:00 1234 IN"};
+        int[] result = solution(fees, records);
+        System.out.println(result);
     }
 
 }
